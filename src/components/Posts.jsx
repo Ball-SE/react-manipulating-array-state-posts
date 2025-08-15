310252b5cd2887ab6e19e29a6ba8c0d4c3575f26
Pharnuwat <se56160411@gmail.com>
2025-08-15 14:18:10 +0700

    feat: add redering list and make the button increase like and dislike

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,52 @@
+import { postData } from "../raw-data/post-data";
+import { useState } from "react";
+
 function Posts() {
+  const [posts, setPosts] = useState(postData);
+
+  const handleAddLike = (likeIndex) => {
+    setPosts((like) =>
+      like.map((post, index) =>
+        index === likeIndex ? { ...post, likes: post.likes + 1 } : post
+      )
+    );
+  };
+
+  const handleAddDislike = (dislikeIndex) => {
+    setPosts((dislike) =>
+      dislike.map((post, index) =>
+        index === dislikeIndex ? { ...post, likes: post.likes - 1 } : post
+      )
+    );
+  };
+
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+        {posts.map((post, index) => (
+          <div class="post-item" key={index}>
+            <div class="post-header">
+              <h2>{post.title}</h2>
+              <div class="post-social-media-stats">
+                <span class="stats-topic">Likes: </span>
+                <span class="post-likes">{post.likes}</span>
+              </div>
+            </div>
+            <p class="post-content">{post.content}</p>
+            <div class="post-actions">
+              <button class="like-button" onClick={() => handleAddLike(index)}>
+                Like
+              </button>
+              <button
+                class="dislike-button"
+                onClick={() => handleAddDislike(index)}
+              >
+                Dislike
+              </button>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
